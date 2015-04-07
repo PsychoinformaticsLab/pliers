bdad46ebcf02ce18bcdddd94ab7eb99f92f88f60
tyarkoni <tyarkoni@gmail.com>
2015-04-06 21:54:27 -0500

    nothing to see here; move along...

---
+++ README.md
@@ -0,0 +1,3 @@
+# Annotations
+
+A Python package for multimodal stimulus annotation. This is just a placeholder for now.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from setuptools import setup
+from annotations.version import __version__
+
+if len(set(('test', 'easy_install')).intersection(sys.argv)) > 0:
+    import setuptools
+
+extra_setuptools_args = {}
+if 'setuptools' in sys.modules:
+    extra_setuptools_args = dict(
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        extras_require=dict(
+            test='nose>=0.10.1')
+    )
+
+setup(
+    name="annotations",
+    version=__version__,
+    description="Multimodal stimulus annotation in Python",
+    author='Tal Yarkoni',
+    author_email='tyarkoni@gmail.com',
+    url='http://github.com/tyarkoni/annotations',
+    packages=["annotations"],
+    package_data={'annotations': ['data/*'],
+                'annotations.tests': ['data/*']
+                },
+    # download_url='https://github.com/tyarkoni/annotations/archive/%s.tar.gz' % __version__,
+    **extra_setuptools_args
+    )
