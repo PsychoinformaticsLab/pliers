57329caab2be32f2fa78929d03eefbe4a23abfc1
tyarkoni <tyarkoni@gmail.com>
2015-04-19 16:20:17 -0500

    smoke test for whole pipeline

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 from utils import get_test_data_path
 from annotations.stims import VideoStim, VideoFrameStim
+from annotations.annotators import (FaceDetectionAnnotator,
+                                    DenseOpticalFlowAnnotator,
+                                    VideoAnnotator)
+from annotations.io import FSLExporter
 from os.path import join
-import numpy as np
+import tempfile
+import shutil
+import pandas as pd
+import cProfile
 
 
 class TestCore(TestCase):
@@ -22,3 +29,18 @@ class TestCore(TestCase):
         self.assertIsInstance(f, VideoFrameStim)
         self.assertIsInstance(f.onset, float)
         self.assertEquals(f.data.shape, (320, 560, 3))
+
+    def test_full_pipeline(self):
+        ''' Smoke test of entire pipeline, from stimulus loading to
+        event file export. '''
+        stim = VideoStim(join(get_test_data_path(), 'video', 'small.mp4'))
+        annotators = [DenseOpticalFlowAnnotator(), FaceDetectionAnnotator()]
+        va = VideoAnnotator(annotators)
+        timeline = va.apply(stim)
+        exp = FSLExporter()
+        tmpdir = tempfile.mkdtemp()
+        exp.export(timeline, tmpdir)
+        from glob import glob
+        files = glob(join(tmpdir, '*.txt'))
+        print len(files)
+        shutil.rmtree(tmpdir)
