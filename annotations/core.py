b64d240ef06cc9bd34d09fb5dc72234239ee6ab0
tyarkoni <tyarkoni@gmail.com>
2015-04-25 15:51:44 -0500

    position --> onset

--- annotations/core.py
+++ annotations/core.py
@@ -14,9 +14,9 @@ class Note(object):
 
 class Event(object):
 
-    def __init__(self, position=None, notes=None, duration=None):
+    def __init__(self, onset=None, notes=None, duration=None):
 
-        self.position = position
+        self.onset = onset
         if notes is None:
             notes = []
         self.notes = notes
@@ -34,48 +34,48 @@ class Timeline(object):
 
         if events is not None:
             for i, ev in enumerate(events):
-                position = ev.position
-                if position is None:
+                onset = ev.onset
+                if onset is None:
                     if period is None:
-                        raise ValueError("Position attribute missing in at least "
+                        raise ValueError("onset attribute missing in at least "
                                          "one event, and no period argument was "
                                          "provided. If events do not all have "
-                                         "valid positions, you must explicitly"
+                                         "valid onsets, you must explicitly"
                                          " specify the periodicity to assume.")
-                    position = period * i
-                self.add_event(ev, position, False)
+                    onset = period * i
+                self.add_event(ev, onset, False)
 
         self._sort_events()
         self.period = period
 
     def _sort_events(self):
-        ''' Sort the events OrderedDict by ascending position. '''
+        ''' Sort the events OrderedDict by ascending onset. '''
         self.events = OrderedDict(sorted(self.events.items(),
                                          key=lambda t: t[0]))
 
-    def add_event(self, event, position=None, sort=True, merge=False):
+    def add_event(self, event, onset=None, sort=True, merge=False):
         '''
         Args:
             event (Event): The Event to add to the Timeline
-            position (float): The position/onset at which to insert the Event.
-                If None, the event must have a valid position property.
-            sort (bool): Wether or not to sort events by position after adding.
+            onset (float): The onset/onset at which to insert the Event.
+                If None, the event must have a valid onset property.
+            sort (bool): Wether or not to sort events by onset after adding.
             merge (bool): If True, all notes in the passed event will be merged
                 with any notes in the existing event (with duplicates ignored).
                 If False (default), new events will overwrite existing ones.
         '''
-        if position is None:
-            position = event.position
+        if onset is None:
+            onset = event.onset
 
-        if position is None:
-            raise ValueError("If no position is specified in add() call, "
-                             "the passed Event must have a valid position "
+        if onset is None:
+            raise ValueError("If no onset is specified in add() call, "
+                             "the passed Event must have a valid onset "
                              "attribute.")
 
-        if position in self.events and merge:
-            event.notes = list(set(event.notes + self.events[position].notes))
+        if onset in self.events and merge:
+            event.notes = list(set(event.notes + self.events[onset].notes))
 
-        self.events[position] = event
+        self.events[onset] = event
 
         if sort:
             self._sort_events()
