adbd6a626c97d7496fa18cb5ee848c8cb43d2201
tyarkoni <tyarkoni@gmail.com>
2015-05-10 18:22:13 -0500

    fixed VideoAnnotator bug

--- annotations/annotators/video.py
+++ annotations/annotators/video.py
@@ -1,7 +1,6 @@
 from annotations import stims
 from annotations.annotators import Annotator
-from annotations.annotators.image import ImageAnnotator
-from annotations.core import Note
+from annotations.core import Note, Event
 import cv2
 import numpy as np
 
@@ -11,28 +10,36 @@ class VideoAnnotator(Annotator):
     target = stims.VideoStim
 
 
-class DenseOpticalFlowAnnotator(ImageAnnotator):
+class DenseOpticalFlowAnnotator(VideoAnnotator):
 
     def __init__(self):
-        self.last_frame = None
         super(self.__class__, self).__init__()
 
-    def apply(self, img, show=False):
-        _img = cv2.cvtColor(img.data, cv2.COLOR_BGR2GRAY)
-        if self.last_frame is None:
-            self.last_frame = _img
-            total_flow = 0
-        else:
-            curr_frame = _img
+    def apply(self, video, show=False):
+
+        events = []
+        for i, f in enumerate(video):
+
+            img = f.data
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            if i == 0:
+                last_frame = img
+                total_flow = 0
+
             flow = cv2.calcOpticalFlowFarneback(
-                self.last_frame, curr_frame, 0.5, 3, 15, 3, 5, 1.2, 0)
-            flow = np.sqrt((flow**2).sum(2))
+                last_frame, img, 0.5, 3, 15, 3, 5, 1.2, 0)
+            flow = np.sqrt((flow ** 2).sum(2))
 
             if show:
                 cv2.imshow('frame', flow.astype('int8'))
                 cv2.waitKey(1)
 
-            self.last_frame = curr_frame
+            last_frame = img
             total_flow = flow.sum()
 
-        return Note(img, self, {'total_flow': total_flow})
+            note = Note(video, self, {'total_flow': total_flow})
+            event = Event(onset=f.onset, duration=f.duration, notes=[note])
+            events.append(event)
+
+        return events
