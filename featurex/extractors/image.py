fb5eb060c93be71e04339e0323b8ce53c9f1aed4
qmac <quinten.mcnamara@gmail.com>
2015-10-01 19:57:23 -0500

    added general image classifier from MM

--- featurex/extractors/image.py
+++ featurex/extractors/image.py
@@ -1,8 +1,11 @@
 from featurex.stimuli import video
 from featurex.extractors import StimExtractor
 import cv2
+import time
 import numpy as np
 from featurex.core import Value
+
+from metamind.api import set_api_key, general_image_classifier
 
 
 class ImageExtractor(StimExtractor):
@@ -79,4 +82,22 @@ class SharpnessExtractor(ImageExtractor):
         
         sharpness = np.max(cv2.convertScaleAbs(cv2.Laplacian(gray_image, 3)))
         return Value(img, self, {'sharpness': sharpness})
+
+class NaturalFeaturesExtractor(ImageExtractor):
+    ''' Uses the MetaMind API to extract features within the general classifier dataset '''
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        set_api_key('1s8nqbHlFfPf82IrDlGFmz2uEXHlSJ6DveJx7r8Ycoz8ahqBwq')
+
+    def apply(self, img):
+        data = img.data
+        temp_file = 'temp.jpg'
+        cv2.imwrite(temp_file, data)
+        labels = general_image_classifier.predict(temp_file, input_type='files')
+        top_label = labels[0]['label']
+        print top_label
+        time.sleep(1.0)
+
+        return Value(img, self, {'labels': labels})
+
 
