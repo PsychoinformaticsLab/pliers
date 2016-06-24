f17b4db3f5026d067020c18f796c59abee8a2875
tyarkoni <tyarkoni@gmail.com>
2016-06-24 15:38:07 +0200

    adds Wit.ai speech transcription; closes #16

--- featurex/extractors/api.py
+++ featurex/extractors/api.py
@@ -3,6 +3,7 @@ Extractors that interact with external (e.g., deep learning) services.
 '''
 
 from featurex.extractors.image import ImageExtractor
+from featurex.extractors.audio import AudioExtractor
 from scipy.misc import imsave
 from featurex.core import Value
 import os
@@ -16,6 +17,11 @@ except ImportError:
 
 try:
     from clarifai.client import ClarifaiApi
+except ImportError:
+    pass
+
+try:
+    import speech_recognition as sr
 except ImportError:
     pass
 
@@ -39,7 +45,7 @@ class ClarifaiAPIExtractor(ImageExtractor):
                 app_id = os.environ['CLARIFAI_APP_ID']
                 app_secret = os.environ['CLARIFAI_APP_SECRET']
             except KeyError:
-                raise ValueError("A valid Clarifai API APP_ID and APP_SECRET"
+                raise ValueError("A valid Clarifai API APP_ID and APP_SECRET "
                                  "must be passed the first time a Clarifai "
                                  "extractor is initialized.")
 
@@ -57,3 +63,28 @@ class ClarifaiAPIExtractor(ImageExtractor):
         os.remove(temp_file)
 
         return Value(img, self, {'tags': tags})
+
+
+class WitTranscriptionExtractor(AudioExtractor):
+    ''' Uses the Wit.AI API (via the SpeechRecognition package) to run speech-
+    to-text transcription on an audio file.
+    Args:
+        api_key (str): Wit.AI API key. Must be passed explicitly or stored in
+            the environment variable WIT_AI_API_KEY.
+    '''
+
+    def __init__(self, api_key=None):
+        if api_key is None:
+            try:
+                api_key = os.environ['WIT_AI_API_KEY']
+            except KeyError:
+                raise ValueError("A valid Wit.AI API key must be passed when "
+                                 "a WitTranscriptionExtractor is initialized.")
+        self.recognizer = sr.Recognizer()
+        self.api_key = api_key
+
+    def apply(self, audio):
+        with sr.AudioFile(audio.filename) as source:
+            clip = self.recognizer.record(source)
+        text = self.recognizer.recognize_wit(clip, self.api_key)
+        return Value(text, self, {'text': text})
