b5efbd51c14538b52439629359029cfa69c5be26
tyarkoni <tyarkoni@gmail.com>
2016-09-08 15:21:38 -0700

    adds Google Cloud Vision API face extractor and shells for other google services

---
+++ featurex/extractors/google.py
@@ -0,0 +1,133 @@
+# export GOOGLE_APPLICATION_CREDENTIALS=/path/to/your/credentials-key.json
+
+import argparse
+import base64
+import os
+import re
+import sys
+from featurex.extractors.image import ImageExtractor
+from featurex.stimuli.image import ImageStim
+from featurex import Value, Event
+import tempfile
+from scipy.misc import imsave
+import numpy as np
+
+try:
+    from googleapiclient import discovery, errors
+    from oauth2client.client import GoogleCredentials
+except ImportError:
+    pass
+
+
+DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+BATCH_SIZE = 10
+
+
+class GoogleAPIExtractor(ImageExtractor):
+
+    def __init__(self, discovery_file=None, api_version='v1', max_results=100,
+                 num_retries=3):
+
+        if discovery_file is None:
+            if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
+                raise ValueError("No Google application credentials found. "
+                    "A JSON service account key must be either passed as the "
+                    "discovery_file argument, or set in the "
+                    "GOOGLE_APPLICATION_CREDENTIALS environment variable.")
+            discovery_file = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+        self.credentials = GoogleCredentials.from_stream(discovery_file)
+        self.max_results = max_results
+        self.num_retries = num_retries
+        self.service = discovery.build(self.api_name, api_version,
+                                       credentials=self.credentials,
+                                       discoveryServiceUrl=DISCOVERY_URL)
+        super(GoogleAPIExtractor, self).__init__()
+
+    def apply(self, stim):
+        if isinstance(stim, ImageStim):
+            is_image = True
+            stim = [stim]
+        request =  self._build_request(stim)
+        responses = self._query_api(request)[self.response_object]
+
+        events = []
+        for i, response in enumerate(responses):
+            value = self._parse_response(stim[i], response)
+            onset = stim[i].onset if hasattr(stim[i], 'onset') else i
+            ev = Event(onset=onset, duration=stim[i].duration, values=[value])
+            events.append(ev)
+
+        if is_image:
+            return events[0].values[0]
+        return events
+
+    def _query_api(self, request):
+        resource = getattr(self.service, self.resource)()
+        request = resource.annotate(body={'requests': request})
+        return request.execute(num_retries=self.num_retries)['responses'][0]
+
+
+class GoogleVisionAPIExtractor(GoogleAPIExtractor):
+
+    api_name = 'vision'
+    resource = 'images'
+
+    def _build_request(self, stim):
+        request = []
+        for image in stim:
+            temp_file = tempfile.mktemp() + '.png'
+            imsave(temp_file, image.data)
+            img_data = open(temp_file, 'rb').read()
+            content = base64.b64encode(img_data).decode()
+            request.append(
+                {
+                'image': { 'content': content },
+                'features': [{
+                    'type': self.request_type,
+                    'maxResults': self.max_results,
+                }]
+            })
+        return request
+
+
+class GoogleVisionAPIFaceExtractor(GoogleVisionAPIExtractor):
+
+    request_type = 'FACE_DETECTION'
+    response_object = 'faceAnnotations'
+
+    def _parse_response(self, stim, response):
+        data_dict = {}
+        for field, val in response.items():
+            if field not in ['boundingPoly', 'fdBoundingPoly', 'landmarks']:
+                data_dict[field] = val
+            elif 'oundingPoly' in field:
+                for i, vertex in enumerate(val['vertices']):
+                    for dim in ['x', 'y']:
+                        name = '%s_vertex%d_%s' % (field, i+1, dim)
+                        val = vertex[dim] if dim in vertex else np.nan
+                        data_dict[name] = val
+            elif field == 'landmarks':
+                for lm in val:
+                    name = 'landmark_' + lm['type'] + '_%s'
+                    lm_pos = { name % k : v for (k, v) in lm['position'].items()}
+                    data_dict.update(lm_pos)
+        return Value(stim=stim, extractor=self, data=data_dict)
+
+
+class GoogleVisionAPITextExtractor(GoogleVisionAPIExtractor):
+
+    request_type = 'TEXT_DETECTION'
+    response_object = 'textAnnotations'
+
+
+class GoogleVisionAPILabelExtractor(GoogleVisionAPIExtractor):
+
+    request_type = 'LABEL_DETECTION'
+    response_object = 'labelAnnotations'
+
+
+class GoogleVisionAPIPropertyExtractor(GoogleVisionAPIExtractor):
+
+    request_type = 'IMAGE_PROPERTIES'
+    response_object = 'imagePropertiesAnnotations'
---
+++ featurex/tests/test_google_extractors.py
@@ -0,0 +1,43 @@
+from featurex.extractors.google import (GoogleVisionAPIExtractor,
+                                        GoogleVisionAPIFaceExtractor)
+from featurex.stimuli.image import ImageStim
+from featurex import Value
+import pytest
+import os
+import json
+from os.path import join
+from .utils import _get_test_data_path
+import numpy as np
+
+
+@pytest.mark.skipif("'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ")
+def test_google_vision_api_extractor_inits():
+    ext = GoogleVisionAPIExtractor(num_retries=5)
+    assert ext.num_retries == 5
+    assert ext.max_results == 100
+    assert ext.service is not None
+
+@pytest.mark.skipif("'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ")
+def test_google_vision_api__face_extractor_inits():
+    ext = GoogleVisionAPIFaceExtractor(num_retries=5)
+    assert ext.num_retries == 5
+    assert ext.max_results == 100
+    assert ext.service is not None
+
+    # Load dummy stim
+    filename = join(_get_test_data_path(), 'image', 'obama.jpg')
+    stim = ImageStim(filename)
+
+    # Test parsing of individual response
+    filename = join(_get_test_data_path(), 'payloads', 'google_vision_api_face_payload.json')
+    response = json.load(open(filename, 'r'))
+    result = ext._parse_response(stim, response['faceAnnotations'][0])
+    assert isinstance(result, Value)
+    assert result.stim == stim
+    assert result.data['angerLikelihood'] == 'VERY_UNLIKELY'
+    assert result.data['landmark_LEFT_EYE_BOTTOM_BOUNDARY_y'] == 257.023
+    assert np.isnan(result.data['boundingPoly_vertex2_y'])
+    values = stim.extract([ext]).data['GoogleVisionAPIFaceExtractor']
+    assert isinstance(values, Value)
+    assert values.data['rollAngle'] == -1.6712251
+    assert values.data['angerLikelihood'] == "VERY_UNLIKELY"
