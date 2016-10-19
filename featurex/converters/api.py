609c16df0709902cf18e2e8b7fb04530ab61b3df
qmac <quinten.mcnamara@gmail.com>
2016-10-19 03:01:58 -0500

    implemented IBM speech API, which includes onset information

--- featurex/converters/api.py
+++ featurex/converters/api.py
@@ -1,5 +1,7 @@
 import os
-from featurex.stimuli.text import ComplexTextStim
+import base64
+import json
+from featurex.stimuli.text import DynamicTextStim, ComplexTextStim
 from featurex.converters.audio import AudioToTextConverter
 
 try:
@@ -7,6 +9,13 @@ try:
 except ImportError:
     pass
 
+try:
+    from urllib import urlencode
+    from urllib2 import Request, urlopen, URLError, HTTPError
+except ImportError:
+    from urllib.parse import urlencode
+    from urllib.request import Request, urlopen
+    from urllib.error import URLError, HTTPError
 
 class SpeechRecognitionConverter(AudioToTextConverter):
     ''' Uses the SpeechRecognition API, which interacts with several APIs 
@@ -43,4 +52,80 @@ class GoogleSpeechAPIConverter(SpeechRecognitionConverter):
 
     environ_key = 'GOOGLE_API_KEY'
     recognize_method = 'recognize_google'
+
+
+class IBMSpeechAPIConverter(AudioToTextConverter):
+    ''' Uses the IBM Watson Text to Speech API to run speech-to-text 
+    transcription on an audio file.
+
+    Args:
+        username (str): API credential username. Must be passed explicitly
+            or stored in the environment variable specified in the environ_key
+            field.
+        password (str): API credential password. Must be passed explicitly
+            or stored in the environment variable specified in the environ_key
+            field.
+    '''
+
+    def __init__(self, username=None, password=None):
+        if username is None or password is None:
+            try:
+                username = os.environ['IBM_USERNAME']
+                password = os.environ['IBM_PASSWORD']
+            except KeyError:
+                raise ValueError("A valid API key must be passed when "
+                                 "a SpeechRecognitionConverter is initialized.")
+        self.recognizer = sr.Recognizer()
+        self.username = username
+        self.password = password
+
+    def _convert(self, audio):
+        with sr.AudioFile(audio.filename) as source:
+            clip = self.recognizer.record(source)
+
+        result = self._query_api(clip)
+
+        timestamps = result['results'][0]['alternatives'][0]['timestamps']
+        elements = []
+        for i, entry in enumerate(timestamps):
+            elements.append(DynamicTextStim(text=entry[0], order=i, onset=entry[1],
+                                                duration=entry[2]-entry[1]))
+        
+        return ComplexTextStim(elements=elements)
+
+
+    def _query_api(self, clip):
+        # Adapted from SpeechRecognition source code, modified to get text onsets
+        flac_data = clip.get_flac_data(
+            convert_rate = None if clip.sample_rate >= 16000 else 16000,
+            convert_width = None if clip.sample_width >= 2 else 2
+        )
+        model = "{0}_BroadbandModel".format("en-US")
+        url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?{0}".format(urlencode({
+            "profanity_filter": "false",
+            "continuous": "true",
+            "model": model,
+            "timestamps": "true",
+        }))
+        request = Request(url, data = flac_data, headers = {
+            "Content-Type": "audio/x-flac",
+            "X-Watson-Learning-Opt-Out": "true",
+        })
+        
+        if hasattr("", "encode"): # Python 2.6 compatibility
+            authorization_value = base64.standard_b64encode("{0}:{1}".format(self.username, self.password).encode("utf-8")).decode("utf-8")
+        else:
+            authorization_value = base64.standard_b64encode("{0}:{1}".format(self.username, self.password))
+        request.add_header("Authorization", "Basic {0}".format(authorization_value))
+        
+        try:
+            response = urlopen(request, timeout=None)
+        except HTTPError as e:
+            raise RequestError("recognition request failed: {0}".format(getattr(e, "reason", "status {0}".format(e.code))))
+        except URLError as e:
+            raise RequestError("recognition connection failed: {0}".format(e.reason))
+        
+        response_text = response.read().decode("utf-8")
+        result = json.loads(response_text)
+        return result
 
--- featurex/tests/test_converters.py
+++ featurex/tests/test_converters.py
@@ -1,7 +1,7 @@
 from os.path import join
 from .utils import get_test_data_path
-from featurex.converters.api import WitTranscriptionConverter, GoogleSpeechAPIConverter
-from featurex.stimuli.text import ComplexTextStim
+from featurex.converters.api import WitTranscriptionConverter, GoogleSpeechAPIConverter, IBMSpeechAPIConverter
+from featurex.stimuli.text import DynamicTextStim, ComplexTextStim
 from featurex.stimuli.audio import AudioStim
 import pytest
 
@@ -25,3 +25,19 @@ def test_googleAPI_converter():
     assert type(out_stim) == ComplexTextStim
     text = [elem.text for elem in out_stim]
     assert 'thermodynamics' in text or 'obey' in text
+
+@pytest.mark.skipif("'IBM_USERNAME' not in os.environ or "
+    "'IBM_PASSWORD' not in os.environ")
+def test_ibmAPI_converter():
+    audio_dir = join(get_test_data_path(), 'audio')
+    stim = AudioStim(join(audio_dir, 'homer.wav'))
+    conv = IBMSpeechAPIConverter()
+    out_stim = conv.transform(stim)
+    assert type(out_stim) == ComplexTextStim
+    first_word = next(w for w in out_stim)
+    assert type(first_word) == DynamicTextStim
+    assert first_word.duration > 0
+    assert first_word.onset != None
+
+    full_text = [elem.text for elem in out_stim]
+    assert 'thermodynamics' in full_text or 'obey' in full_text
