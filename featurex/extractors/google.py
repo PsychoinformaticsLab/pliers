e5e5ee0c3f7f7438a2519d32458fa72ea7af98e4
qmac <quinten.mcnamara@gmail.com>
2016-10-23 18:08:42 -0500

    updated google extractors and tests

--- featurex/extractors/google.py
+++ featurex/extractors/google.py
@@ -1,6 +1,7 @@
 from featurex.extractors.image import ImageExtractor
 from featurex.stimuli.image import ImageStim
 from featurex.google import GoogleVisionAPITransformer
+from featurex.extractors import ExtractorResult
 from featurex import Value, Event
 import numpy as np
 
@@ -9,41 +10,48 @@ class GoogleVisionAPIExtractor(GoogleVisionAPITransformer, ImageExtractor):
 
     def _extract(self, stim):
         if isinstance(stim, ImageStim):
-            is_image = True
             stim = [stim]
-        else:
-            is_image = False
+
         request =  self._build_request(stim)
         responses = self._query_api(request)
 
-        events = []
+        features = []
+        data = []
         for i, response in enumerate(responses):
             if response:
                 annotations = response[self.response_object]
-                values = self._parse_annotations(stim[i], annotations)
-                onset = stim[i].onset if hasattr(stim[i], 'onset') else i
-                ev = Event(onset=onset, duration=stim[i].duration, values=values)
-                events.append(ev)
-            else:
-                events.append(Event())
+                feat, values = self._parse_annotations(annotations)
+                features += feat
+                data += values
 
-        if is_image:
-            return events[0].values
-        return events
+        data = [data]
+        onsets = [stim[i].onset if hasattr(stim[i], 'onset') else i for i in range(len(responses))]
+        durations = [stim[i].duration for i in range(len(responses))]
+        return ExtractorResult(data, stim, self, features=features, 
+                                onsets=onsets, durations=durations)
 
 
 class GoogleVisionAPIFaceExtractor(GoogleVisionAPIExtractor):
 
     request_type = 'FACE_DETECTION'
     response_object = 'faceAnnotations'
+    likelihood_dict = {'UNKNOWN': 0.0, 
+                        'VERY_UNLIKELY': 0.1, 
+                        'UNLIKELY': 0.3, 
+                        'POSSIBLE': 0.5, 
+                        'LIKELY': 0.7, 
+                        'VERY_LIKELY': 0.9}
 
-    def _parse_annotations(self, stim, annotations):
+    def _parse_annotations(self, annotations):
+        features = []
         values = []
         for annotation in annotations:
             data_dict = {}
             for field, val in annotation.items():
-                if field not in ['boundingPoly', 'fdBoundingPoly', 'landmarks']:
-                    data_dict[field] = val
+                if 'Likelihood' in field:
+                    data_dict[field] = self.likelihood_dict[val]
+                elif 'Confidence' in field:
+                    data_dict['face_' + field] = val
                 elif 'oundingPoly' in field:
                     for i, vertex in enumerate(val['vertices']):
                         for dim in ['x', 'y']:
@@ -55,8 +63,12 @@ class GoogleVisionAPIFaceExtractor(GoogleVisionAPIExtractor):
                         name = 'landmark_' + lm['type'] + '_%s'
                         lm_pos = { name % k : v for (k, v) in lm['position'].items()}
                         data_dict.update(lm_pos)
-            values.append(Value(stim=stim, extractor=self, data=data_dict))
-        return values
+                else:
+                    data_dict[field] = val
+            features += data_dict.keys()
+            values += data_dict.values()
+
+        return features, values
 
 
 class GoogleVisionAPILabelExtractor(GoogleVisionAPIExtractor):
@@ -64,12 +76,13 @@ class GoogleVisionAPILabelExtractor(GoogleVisionAPIExtractor):
     request_type = 'LABEL_DETECTION'
     response_object = 'labelAnnotations'
 
-    def _parse_annotations(self, stim, annotations):
+    def _parse_annotations(self, annotations):
+        features = []
         values = []
         for annotation in annotations:
-            data_dict = {field : val for field, val in annotation.items()}
-            values.append(Value(stim=stim, extractor=self, data=data_dict))
-        return values
+            features.append(annotation['description'])
+            values.append(annotation['score'])
+        return features, values
 
 
 class GoogleVisionAPIPropertyExtractor(GoogleVisionAPIExtractor):
@@ -77,6 +90,12 @@ class GoogleVisionAPIPropertyExtractor(GoogleVisionAPIExtractor):
     request_type = 'IMAGE_PROPERTIES'
     response_object = 'imagePropertiesAnnotation'
 
-    def _parse_annotations(self, stim, annotation):
-        data_dict = {field : val for field, val in annotation.items()}
-        return [Value(stim=stim, extractor=self, data=data_dict)]
+    def _parse_annotations(self, annotation):
+        colors = annotation['dominantColors']['colors']
+        features = []
+        values = []
+        for color in colors:
+            rgb = color['color']
+            features.append((rgb['red'], rgb['green'], rgb['blue']))
+            values.append(color['score'])
+        return features, values
