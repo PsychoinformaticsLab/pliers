a00c352ebf7c726507da18c95a0d32ab743eda40
tyarkoni <tyarkoni@gmail.com>
2016-12-27 10:35:04 -0500

    cache Filter results and ensure type matching

--- pliers/filters/__init__.py
+++ pliers/filters/__init__.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from pliers.transformers import Transformer
 from six import with_metaclass
+from pliers.utils import memory
 
 
 __all__ = []
@@ -9,8 +10,16 @@ __all__ = []
 class Filter(with_metaclass(ABCMeta, Transformer)):
     ''' Base class for Filters.'''
 
+    def __init__(self):
+        super(Filter, self).__init__()
+        self.filter = memory.cache(self.filter)
+
     def filter(self, stim, *args, **kwargs):
-        return self._filter(stim, *args, **kwargs)
+        new_stim = self._filter(stim, *args, **kwargs)
+        if not isinstance(new_stim, stim.__class__):
+            raise ValueError("Filter must return a Stim of the same type as "
+                             "its input.")
+        return new_stim
 
     @abstractmethod
     def _filter(self, stim):
