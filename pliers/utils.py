88d6d30dc946a5fedf2a6fb3d32a78c58e033613
tyarkoni <tyarkoni@gmail.com>
2017-01-08 14:32:01 -0600

    adds classproperty decorator

--- pliers/utils.py
+++ pliers/utils.py
@@ -20,3 +20,14 @@ def flatten(l):
                 yield sub
         else:
             yield el
+
+
+class classproperty(object):
+    ''' Implements a @classproperty decorator analogous to @classmethod.
+    Solution from: http://stackoverflow.com/questions/128573/using-property-on-classmethodss
+    '''
+    def __init__(self, fget):
+        self.fget = fget
+
+    def __get__(self, owner_self, owner_cls):
+        return self.fget(owner_cls)
