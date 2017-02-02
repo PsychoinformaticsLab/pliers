3214174676897f87eca3b7360ecf8e5d2f2192d7
tyarkoni <tyarkoni@gmail.com>
2017-02-02 09:48:00 -0600

    add progress bar test

---
+++ pliers/tests/test_utils.py
@@ -0,0 +1,28 @@
+from pliers.utils import progress_bar_wrapper
+from pliers.stimuli import VideoStim
+from pliers.converters import FrameSamplingConverter
+from pliers import config
+from .utils import get_test_data_path
+from tqdm import tqdm
+from os.path import join
+
+
+def test_progress_bar(capfd):
+
+    video_dir = join(get_test_data_path(), 'video')
+    video = VideoStim(join(video_dir, 'obama_speech.mp4'))
+    conv = FrameSamplingConverter(hertz=2)
+
+    old_val = config.progress_bar
+    config.progress_bar = True
+
+    derived = conv.transform(video)
+    out, err = capfd.readouterr()
+    assert 'Video frame:' in err and '100%' in err
+
+    config.progress_bar = False
+    derived = conv.transform(video)
+    out, err = capfd.readouterr()
+    assert 'Video frame:' not in err and '100%' not in err
+
+    config.progress_bar = old_val
