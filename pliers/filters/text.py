403aec866f80b548451667ca26c10280d35290bc
tyarkoni <tyarkoni@gmail.com>
2017-02-21 14:46:32 -0600

    adds WordStemmingFilter; closes #129

---
+++ pliers/filters/text.py
@@ -0,0 +1,50 @@
+from pliers.stimuli.text import TextStim
+from .base import Filter
+from six import string_types
+from nltk import stem
+
+
+class TextFilter(Filter):
+
+    _input_type = TextStim
+
+
+class WordStemmingFilter(TextFilter):
+    ''' Nltk-based word stemming Filter. 
+    Args:
+        stemmer (str, Stemmer): If a string, must be the name of one of the
+            stemming modules available in nltk.stem. Valid values are
+            'porter', 'snowball', 'isri', 'lancaster', 'regexp', 'wordet',
+            or 'rslp'. Alternatively, an initialized nltk StemmerI instance
+            can be passed.
+        args, kwargs: Optional positional and keyword args passed onto the
+            nltk stemmer.
+    '''
+
+    stemmers = {
+        'porter': 'PorterStemmer',
+        'snowball': 'SnowballStemmer',
+        'lancaster': 'LancasterStemmer',
+        'isri': 'ISRIStemmer',
+        'regexp': 'RegexpStemmer',
+        'wordnet': 'WordNetLemmatizer',
+        'rslp': 'RSLPStemmer'
+    }
+
+    def __init__(self, stemmer='porter', *args, **kwargs):
+
+        if isinstance(stemmer, string_types):
+            if not stemmer in self.stemmers:
+                valid = list(self.stemmers.keys())
+                raise ValueError("Invalid stemmer '%s'; please use one of %s." %
+                                 (stemmer, valid))
+            stemmer = getattr(stem, self.stemmers[stemmer])(*args, **kwargs)
+        elif not isinstance(stemmer, (stem.StemmerI, stem.WordNetLemmatizer)):
+            raise ValueError("stemmer must be either a valid string, or an "
+                             "instance of class StemmerI.")
+        self.stemmer = stemmer
+        super(WordStemmingFilter, self).__init__()
+
+    def _filter(self, stim):
+        stemmed = self.stemmer.stem(stim.text)
+        return TextStim(stim.filename, stemmed, stim.onset, stim.duration)
---
+++ pliers/tests/test_filters.py
@@ -0,0 +1,41 @@
+from os.path import join
+import os
+from .utils import get_test_data_path
+from pliers.filters import WordStemmingFilter
+from pliers.stimuli import TextStim, ComplexTextStim
+from nltk import stem as nls
+import pytest
+
+
+TEXT_DIR = join(get_test_data_path(), 'text')
+
+
+def test_word_stemming_filter():
+    stim = ComplexTextStim(join(TEXT_DIR, 'sample_text.txt'),
+                           columns='to', default_duration=1)
+
+    # With all defaults (porter stemmer)
+    filt = WordStemmingFilter()
+    assert isinstance(filt.stemmer, nls.PorterStemmer)
+    stemmed = filt.transform(stim)
+    stems = [s.text for s in stemmed]
+    target = ['some', 'sampl', 'text', 'for', 'test', 'annot']
+    assert stems == target
+
+    # Try a different stemmer
+    filt = WordStemmingFilter(stemmer='snowball', language='english')
+    assert isinstance(filt.stemmer, nls.SnowballStemmer)
+    stemmed = filt.transform(stim)
+    stems = [s.text for s in stemmed]
+    assert stems == target
+
+    # Handles StemmerI stemmer
+    stemmer = nls.WordNetLemmatizer()
+    filt = WordStemmingFilter(stemmer=stemmer)
+    stemmed = filt.transform(stim)
+    stems = [s.text for s in stemmed]
+    assert stems == target
+
+    # Fails on invalid values
+    with pytest.raises(ValueError):
+        filt = WordStemmingFilter(stemmer='nonexistent_stemmer')
