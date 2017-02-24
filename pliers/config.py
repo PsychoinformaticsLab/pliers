16b1f2848c0909f6bfa4de0124f0663711ba2732
tyarkoni <tyarkoni@gmail.com>
2017-02-24 14:56:50 -0600

    disable parallelization by default

--- pliers/config.py
+++ pliers/config.py
@@ -2,7 +2,7 @@ cache_transformers = True
 log_transformations = True
 drop_bad_extractor_results = True
 progress_bar = True
-parallelize = True
+parallelize = False
 n_jobs = None
 default_converters = {
     'AudioStim->TextStim': ('IBMSpeechAPIConverter', 'WitTranscriptionConverter'),
