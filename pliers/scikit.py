49f1b78644d5bc868b50919f997e653b18c929f2
qmac <quinten.mcnamara@gmail.com>
2017-05-06 21:32:18 -0500

    PR feedback edits

--- pliers/scikit.py
+++ pliers/scikit.py
@@ -1,12 +1,18 @@
 from pliers.extractors import Extractor, merge_results
+from pliers.transformers import get_transformer
+from six import string_types
 
 try:
     from sklearn.base import TransformerMixin, BaseEstimator
+
+    class SklearnBase(TransformerMixin, BaseEstimator):
+        pass
 except:
-    TransformerMixin, BaseEstimator = None
+    class SklearnBase():
+        pass
 
 
-class PliersTransformer(BaseEstimator, TransformerMixin):
+class PliersTransformer(SklearnBase):
 
     ''' Simple wrapper for using pliers within a sklearn workflow.
     Args:
@@ -16,8 +22,10 @@ class PliersTransformer(BaseEstimator, TransformerMixin):
     '''
 
     def __init__(self, transformer):
-        self.transformer = transformer
-        self.metadata = None
+        if isinstance(transformer, string_types):
+            self.transformer = get_transformer(transformer)
+        else:
+            self.transformer = transformer
 
     def fit(self, X, y=None):
         return self
@@ -35,7 +43,7 @@ class PliersTransformer(BaseEstimator, TransformerMixin):
         extra_columns = list(set(['onset', 'duration', 'history', 'class',
                                   'filename', 'stim_name', 'source_file']) &
                              set(result.columns))
-        self.metadata = result[extra_columns]
+        self.metadata_ = result[extra_columns]
         result.drop(extra_columns, axis=1, inplace=True, errors='ignore')
 
         return result.as_matrix()
--- pliers/tests/test_scikit.py
+++ pliers/tests/test_scikit.py
@@ -8,8 +8,6 @@ from pliers.extractors import BrightnessExtractor, SharpnessExtractor
 from pliers.graph import Graph
 from pliers.scikit import PliersTransformer
 from pliers.stimuli import ImageStim
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import Normalizer
 
 
 def test_graph_scikit():
@@ -24,7 +22,7 @@ def test_graph_scikit():
     res = trans.fit_transform([stim1, stim2])
     assert res.shape == (2, 1)
     assert res[0][0] == 4 or res[1][0] == 4
-    meta = trans.metadata
+    meta = trans.metadata_
     assert 'history' in meta.columns
     assert meta['history'][1] == 'ImageStim->TesseractConverter/TextStim'
 
@@ -38,13 +36,21 @@ def test_extractor_scikit():
     res = trans.fit_transform(stim)
     assert res.shape == (1, 1)
     assert np.isclose(res[0][0], 0.88784294, 1e-5)
-    meta = trans.metadata
+    meta = trans.metadata_
+    assert np.isnan(meta['onset'][0])
+    trans = PliersTransformer('BrightnessExtractor')
+    res = trans.fit_transform(stim)
+    assert res.shape == (1, 1)
+    assert np.isclose(res[0][0], 0.88784294, 1e-5)
+    meta = trans.metadata_
     assert np.isnan(meta['onset'][0])
 
 
 def test_within_pipeline():
     pytest.importorskip('cv2')
     pytest.importorskip('sklearn')
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import Normalizer
     stim = join(get_test_data_path(), 'image', 'apple.jpg')
     graph = Graph([BrightnessExtractor(), SharpnessExtractor()])
     trans = PliersTransformer(graph)
@@ -54,6 +60,6 @@ def test_within_pipeline():
     assert res.shape == (1, 2)
     assert np.isclose(res[0][0], 0.66393, 1e-5)
     assert np.isclose(res[0][1], 0.74780, 1e-5)
-    meta = trans.metadata
+    meta = trans.metadata_
     assert np.isnan(meta['onset'][0])
     assert meta['class'][0] == 'ImageStim'
