5e22dcc066774a7b2c0bb69d7d8053df8fab164c
qmac <quinten.mcnamara@gmail.com>
2017-06-28 17:11:35 -0500

    refactored into TweetFactory and added comments

--- pliers/stimuli/api.py
+++ pliers/stimuli/api.py
@@ -8,17 +8,27 @@ from .video import VideoStim
 from pliers.transformers import EnvironmentKeyMixin
 
 
-class TweetStim(CompoundStim, EnvironmentKeyMixin):
+class TweetFactory(EnvironmentKeyMixin):
 
-    _allowed_types = (TextStim, ImageStim, VideoStim)
-    _allow_multiple = True
-    _primary = TextStim
+    '''
+    An object from which to generate TweetStims, creates an Api instance from
+    the python-twitter library
+
+    Args:
+        consumer_key (str): A valid consumer key for the Twitter API
+        consumer_secret (str): A valid consumer secret key for the Twitter API
+        access_token_key (str): A valid access token for the Twitter API
+        access_token_secret (str): A valid access token secret for the
+            Twitter API
+
+    To get these credentials, visit https://dev.twitter.com/.
+    '''
+
     _env_keys = ('TWITTER_CONSUMER_KEY', 'TWITTER_CONSUMER_SECRET',
                  'TWITTER_ACCESS_TOKEN_KEY', 'TWITTER_ACCESS_TOKEN_SECRET')
 
     def __init__(self, consumer_key=None, consumer_secret=None,
-                 access_token_key=None, access_token_secret=None,
-                 status_id=None):
+                 access_token_key=None, access_token_secret=None):
         import twitter
 
         if consumer_key is None or consumer_secret is None or \
@@ -38,9 +48,30 @@ class TweetStim(CompoundStim, EnvironmentKeyMixin):
                                access_token_key=access_token_key,
                                access_token_secret=access_token_secret)
         self.api.VerifyCredentials()
-        self.status = self.api.GetStatus(status_id)
-        elements = [TextStim(text=self.status.text)]
-        if self.status.media:
-            media_stims = load_stims([m.media_url for m in self.status.media])
+
+    def get_status(self, status_id):
+        status = self.api.GetStatus(status_id)
+        return TweetStim(status)
+
+
+class TweetStim(CompoundStim):
+
+    '''
+    Represents the text and associated media from a single tweet.
+
+    Args:
+        status (python-twitter Status objcet): the Status from which to
+            extract information, can either be generated from the TweetFactory
+            or user-provided.
+    '''
+
+    _allowed_types = (TextStim, ImageStim, VideoStim)
+    _allow_multiple = True
+    _primary = TextStim
+
+    def __init__(self, status):
+        elements = [TextStim(text=status.text)]
+        if status.media:
+            media_stims = load_stims([m.media_url for m in status.media])
             elements.extend(media_stims)
         super(TweetStim, self).__init__(elements=elements)
