055e97d24a10ab29f19f9eba5df6a022dc25c968
qmac <quinten.mcnamara@gmail.com>
2017-08-10 23:33:35 -0500

    changed to type check first if filter produced iterable

--- pliers/filters/base.py
+++ pliers/filters/base.py
@@ -3,6 +3,7 @@
 from abc import ABCMeta, abstractmethod
 from six import with_metaclass
 from pliers.transformers import Transformer
+from pliers.utils import listify
 
 
 class Filter(with_metaclass(ABCMeta, Transformer)):
@@ -12,7 +13,12 @@ class Filter(with_metaclass(ABCMeta, Transformer)):
         super(Filter, self).__init__()
 
     def _transform(self, stim, *args, **kwargs):
-        return self._filter(stim, *args, **kwargs)
+        new_stim = self._filter(stim, *args, **kwargs)
+        if not isinstance(new_stim, stim.__class__) and \
+           not isinstance(listify(new_stim)[0], stim.__class__):
+            raise ValueError("Filter must return a Stim of the same type as "
+                             "its input.")
+        return new_stim
 
     @abstractmethod
     def _filter(self, stim):
