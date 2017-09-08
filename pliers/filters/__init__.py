2e7c969b64af9e15eba4c5689bb04e5f9c10ee1e
qmac <quinten.mcnamara@gmail.com>
2017-09-08 16:06:19 -0500

    fixed unclean merge

--- pliers/filters/__init__.py
+++ pliers/filters/__init__.py
@@ -1,24 +1,17 @@
 ''' Filter hierarchy. '''
 
-<<<<<<< HEAD
 from .image import (ImageCroppingFilter,
                     PillowImageFilter)
-from .text import WordStemmingFilter
-
-__all__ = [
-    'ImageCroppingFilter',
-    'PillowImageFilter',
-    'WordStemmingFilter'
-=======
 from .text import (WordStemmingFilter,
                    TokenizingFilter,
                    TokenRemovalFilter,
                    PunctuationRemovalFilter)
 
 __all__ = [
+    'ImageCroppingFilter',
+    'PillowImageFilter',
     'WordStemmingFilter',
     'TokenizingFilter',
     'TokenRemovalFilter',
     'PunctuationRemovalFilter'
->>>>>>> master
 ]
