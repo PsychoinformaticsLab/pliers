8fcb7565627bafb5f92b755a33d2b7cc4fbebd80
adelavega <delavega@utexas.edu>
2017-11-30 18:21:51 -0600

    Support converters and filters in updater

--- pliers/tests/test_updater.py
+++ pliers/tests/test_updater.py
@@ -1,18 +1,25 @@
 from pliers.utils.updater import check_updates
-from .utils import DummyExactExtractor
 from tempfile import NamedTemporaryFile
+import pandas as pd
 
 def test_updater():
     datastore_file = NamedTemporaryFile().name
     # Run updater once
-    results = check_updates([DummyExactExtractor(5)], datastore=datastore_file)
-    assert results == {'changed_extractors': None, 'mismatches': None}
+    results = check_updates([('BrightnessExtractor', {})],
+                            datastore=datastore_file)
+    assert results == {'transformers': [], 'mismatches': []}
 
     # Run again with same values
-    results = check_updates([DummyExactExtractor(5)], datastore=datastore_file)
-    assert results == {'changed_extractors': None, 'mismatches': None}
+    results = check_updates([('BrightnessExtractor', {})],
+                            datastore=datastore_file)
+    assert results == {'transformers': [], 'mismatches': []}
 
-    # Change value
-    results = check_updates([DummyExactExtractor(6)], datastore=datastore_file)
-    assert results['changed_extractors'] == {'DummyExactExtractor'}
-    assert len(results['mismatches']) > 10
+    # Change value in datastore
+    ds = pd.read_csv(datastore_file)
+    ds.iloc[1, 3] = 1
+    ds.to_csv(datastore_file, index=False)
+
+    results = check_updates([('BrightnessExtractor', {})],
+                            datastore=datastore_file)
+    assert results['transformers'] == [('BrightnessExtractor', {})]
+    assert len(results['mismatches']) == 1
--- pliers/utils/updater.py
+++ pliers/utils/updater.py
@@ -1,80 +1,112 @@
 """ Utility to check if results have changed in foreign APIs """
 import glob
 import datetime
-import warnings
 import pandas as pd
 import numpy as np
 from os.path import realpath, join, dirname, exists, expanduser
 
 from pliers.stimuli import load_stims
-from pliers.extractors import merge_results
+from pliers.converters import Converter
+from pliers.filters.base import Filter
 
-def check_updates(extractors, datastore=None, stimuli=None):
-    """ Run graph_spec on set of stimuli, and store results in datastore csv.
-    If file exists, compare results to previous in file,
-    and return if any features have changed
+import pliers.extractors
+import pliers.converters
+import pliers.filters
+
+import hashlib
+
+def hash_data(data, blocksize=65536):
+    """" Hashes list of data, strings or data """
+    if isinstance(data, str):
+        data = [data.encode('utf-8')]
+    elif not isinstance(data, list):
+        data = [data]
+
+    hasher = hashlib.sha1()
+    for i in data:
+        hasher.update(i)
+
+    return hasher.hexdigest()
+
+def check_updates(transformers, datastore=None, stimuli=None):
+    """ Run transformers through a battery of stimuli, and check if output has
+    changed. Store results in csv file for comparison.
     Args:
-        extractors - A list of extractor objets.
+        transformers - A list of tuples of transformer names and dictionary of
+                       parameters to instantiate with (or empty dict)
         datastore - filepath of CSV file with results. Stored in home dir
                     by default
         stimuli - list of stimuli file paths to extract from.
                   if None, use test data.
     """
-    # Check if file exists
-    if datastore is None:
-        datastore = expanduser('~/.pliers_updates')
-    if not exists(datastore):
-        prior_data = None
-    else:
-        prior_data = pd.read_csv(datastore)
+    # Find datastore file
+    datastore = datastore or expanduser('~/.pliers_updates')
+    prior_data = pd.read_csv(datastore) if exists(datastore) else None
 
-    if stimuli is None:
-        stimuli = glob.glob(join(
-            dirname(
-                realpath(__file__)), '../tests/data/image/CC0/*'))
-
-    # Load stimuli and extract new features
+    # Load stimuli
+    stimuli = stimuli or glob.glob(
+        join(dirname(realpath(__file__)), '../tests/data/image/CC0/*'))
     stimuli = load_stims(stimuli)
 
-    results = []
-    for stim in stimuli:
-        for ext in extractors:
-            results.append(ext.transform(stim))
+    # Instantiate transformers
+    loaded_transformers = {}
+    for name, parameters in transformers:
+        for source in [pliers.converters, pliers.filters, pliers.extractors]:
+            if hasattr(source, name):
+                trans = getattr(source, name)(**parameters)
+                loaded_transformers[trans] = (name, parameters)
+                break
+        else:
+            raise ValueError("Transformer name could not be found")
 
-    ## Merge results
-    results = merge_results(results).drop(
-        ['source_file', 'filename', 'history', 'class', 'onset', 'duration'],
-        axis=1, level=0)
-    results['time_extracted'] = datetime.datetime.now()
+    # Transform stimuli
+    results = pd.DataFrame({'time_extracted':[datetime.datetime.now()]})
+    for trans in loaded_transformers.keys():
+        for stim in stimuli:
+            if trans._stim_matches_input_types(stim):
+                res = trans.transform(stim)
 
-    # Flatten columns and make into one row
-    results = results.pivot(columns='stim_name', index='time_extracted')
-    results.columns = ['.'.join(col).strip() \
-     if col[1] is not '' else col[0] for col in results.columns.values]
-    results = results.reset_index()
+                try: # Add iterable
+                    res = [res.data for r in res]
+                except TypeError:
+                    res = [r.data for r in res.elements] \
+                          if hasattr(res, 'elements') \
+                          else res.data
 
-    # If new, record data
+                res = hash_data(res) if isinstance(trans, (Converter, Filter)) \
+                      else res[0][0]
+
+                results["{}.{}".format(trans.__hash__(), stim.name)] = [res]
+
+    # Check for mistmatches
     mismatches = []
-    if prior_data is None:
-        results.to_csv(datastore, index=False)
-        warnings.warn("Datastore not found. Initiated new.")
-    else: # Otherwise look for mistmatches
+    if prior_data is not None:
         last = prior_data[
             prior_data.time_extracted == prior_data.time_extracted.max()].\
-            iloc[0]
+            iloc[0].drop('time_extracted')
 
         for label, value in results.iteritems():
-            if label != 'time_extracted':
-                old = last.get(label)
-                new = value.values[0]
+            old = last.get(label)
+            new = value.values[0]
 
-                if old is not None and not np.isclose(old, new):
+            if old is not None:
+                if isinstance(new, str):
+                    if new != old:
+                        mismatches.append(label)
+                elif not np.isclose(old, new):
                     mismatches.append(label)
 
-        new_data = prior_data.append(results)
-        new_data.to_csv(datastore, index=False)
+        results = prior_data.append(results)
 
-    extractors = set([m.split('.')[0] for m in mismatches])
+    results.to_csv(datastore, index=False)
 
-    return {'changed_extractors' : extractors or None,
-            'mismatches' : mismatches or None}
+    # Get corresponding transformer name and parameters
+    def get_trans(hash_tr):
+        for obj, attr in loaded_transformers.items():
+            if str(obj.__hash__()) == hash_tr:
+                return attr
+
+    delta_t = set([m.split('.')[0] for m in mismatches])
+    delta_t = [get_trans(dt) for dt in delta_t]
+
+    return {'transformers': delta_t, 'mismatches': mismatches}
