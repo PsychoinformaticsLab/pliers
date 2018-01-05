4fa0f891305e49070034f3c73839f478b92a8090
Tal Yarkoni <tyarkoni@gmail.com>
2018-01-05 15:51:34 -0600

    brings back to_long_format()--now in utils

--- pliers/tests/test_io.py
+++ pliers/tests/test_io.py
@@ -61,3 +61,24 @@ def test_convert_to_long_graph():
     assert 'feature' in long_timeline.columns
     assert 'extractor' in long_timeline.columns
     assert 'history' in long_timeline.columns
+
+
+def test_convert_to_long():
+    # Avoids circular import
+    from pliers.utils.io import to_long_format
+    audio_dir = join(get_test_data_path(), 'audio')
+    stim = AudioStim(join(audio_dir, 'barber.wav'))
+    ext = STFTAudioExtractor(frame_size=1., spectrogram=False,
+                             freq_bins=[(100, 300), (300, 3000),
+                                        (3000, 20000)])
+    timeline = ext.transform(stim)
+    long_timeline = to_long_format(timeline)
+    assert long_timeline.shape == (timeline.to_df().shape[0] * 3, 4)
+    assert 'feature' in long_timeline.columns
+    assert 'value' in long_timeline.columns
+    assert '100_300' not in long_timeline.columns
+    timeline = ExtractorResult.merge_features([timeline])
+    long_timeline = to_long_format(timeline)
+    assert 'feature' in long_timeline.columns
+    assert 'extractor' in long_timeline.columns
+    assert '100_300' not in long_timeline.columns
--- pliers/utils/__init__.py
+++ pliers/utils/__init__.py
@@ -3,6 +3,7 @@
 from .base import (listify, flatten, batch_iterable, classproperty, isiterable,
                    isgenerator, progress_bar_wrapper, attempt_to_import,
                    EnvironmentKeyMixin, verify_dependencies, set_iterable_type)
+from .io import to_long_format
 
 
 __all__ = [
@@ -16,5 +17,6 @@ __all__ = [
     'attempt_to_import',
     'EnvironmentKeyMixin',
     'verify_dependencies',
-    'set_iterable_type'
+    'set_iterable_type',
+    'to_long_format'
 ]
