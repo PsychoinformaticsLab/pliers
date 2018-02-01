2a3af29f3f82e12d610619d93cbcb6f74965caea
Tal Yarkoni <tyarkoni@gmail.com>
2018-01-31 22:39:57 -0600

    assume the best, I guess

--- ci/decrypt.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-if [[ -v GOOGLE_APPLICATION_CREDENTIALS ]]; then
-  openssl aes-256-cbc -K $encrypted_a0a62c26415d_key -iv $encrypted_a0a62c26415d_iv
-  -in pliers/tests/credentials/google.json.enc -out pliers/tests/credentials/google.json -d;
- fi
