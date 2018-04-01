c649f31d78131c1a709d1f41a71b05a2932c1dd0
qmac <quinten.mcnamara@gmail.com>
2018-03-31 19:20:31 -0500

    clarifai video extraction

--- pliers/extractors/api/clarifai.py
+++ pliers/extractors/api/clarifai.py
@@ -8,6 +8,7 @@ try:
 except Exception as e:
     from contextlib2 import ExitStack
 from pliers.extractors.image import ImageExtractor
+from pliers.extractors.video import VideoExtractor
 from pliers.extractors.base import ExtractorResult
 from pliers.transformers import BatchTransformerMixin
 from pliers.transformers.api import APITransformer
@@ -19,11 +20,11 @@ clarifai_client = attempt_to_import('clarifai.rest.client', 'clarifai_client',
                                      'Concept',
                                      'ModelOutputConfig',
                                      'ModelOutputInfo',
-                                     'Image'])
+                                     'Image',
+                                     'Video'])
 
 
-class ClarifaiAPIExtractor(APITransformer, BatchTransformerMixin,
-                           ImageExtractor):
+class ClarifaiAPIExtractor(APITransformer):
 
     ''' Uses the Clarifai API to extract tags of images.
 
@@ -42,7 +43,6 @@ class ClarifaiAPIExtractor(APITransformer, BatchTransformerMixin,
 
     _log_attributes = ('api_key', 'model', 'model_name', 'min_value',
                        'max_concepts', 'select_concepts')
-    _batch_size = 128
     _env_keys = ('CLARIFAI_API_KEY',)
     VERSION = '1.0'
 
@@ -81,27 +81,67 @@ class ClarifaiAPIExtractor(APITransformer, BatchTransformerMixin,
     def check_valid_keys(self):
         return self.api is not None
 
-    def _extract(self, stims):
+    def _query_api(self, objects):
         verify_dependencies(['clarifai_client'])
         moc = clarifai_client.ModelOutputConfig(min_value=self.min_value,
                                                 max_concepts=self.max_concepts,
                                                 select_concepts=self.select_concepts)
         output_config = moc
         model_output_info = clarifai_client.ModelOutputInfo(output_config=output_config)
+        tags = self.model.predict(objects, model_output_info=model_output_info)
+        return tags['outputs']
+
+    def _parse_annotations(self, annotation):
+        data_dict = {}
+        for tag in annotation['data']['concepts']:
+            data_dict[tag['name']] = tag['value']
+        return data_dict
+
+
+class ClarifaiAPIImageExtractor(ClarifaiAPIExtractor, BatchTransformerMixin,
+                                ImageExtractor):
+
+    _batch_size = 128
+
+    def _extract(self, stims):
+        verify_dependencies(['clarifai_client'])
 
         # ExitStack lets us use filename context managers simultaneously
         with ExitStack() as stack:
             files = [stack.enter_context(s.get_filename()) for s in stims]
             imgs = [clarifai_client.Image(filename=filename) for filename in files]
-            tags = self.model.predict(imgs, model_output_info=model_output_info)
+            outputs = self._query_api(imgs)
 
-        extracted = []
-        for i, resp in enumerate(tags['outputs']):
-            extracted.append(ExtractorResult(resp, stims[i], self))
-        return extracted
+        extractions = []
+        for i, resp in enumerate(outputs):
+            extractions.append(ExtractorResult(resp, stims[i], self))
+        return extractions
 
     def _to_df(self, result):
-        data_dict = {}
-        for tag in result._data['data']['concepts']:
-            data_dict[tag['name']] = tag['value']
-        return pd.DataFrame([data_dict])
+        return pd.DataFrame([self._parse_annotations(result._data)])
+
+
+class ClarifaiAPIVideoExtractor(ClarifaiAPIExtractor, VideoExtractor):
+
+    def _extract(self, stim):
+        verify_dependencies(['clarifai_client'])
+        with stim.get_filename() as filename:
+            vids = [clarifai_client.Video(filename=filename)]
+            outputs = self._query_api(vids)
+        return ExtractorResult(outputs, stim, self)
+
+    def _to_df(self, result):
+        rows = []
+        for frame_res in result._data[0]['data']['frames']:
+            data_dict = self._parse_annotations(frame_res)
+            data_dict['onset_'] = frame_res['frame_info']['time'] / 1000.0
+            frame_num = frame_res['frame_info']['index']
+            if frame_num == 0:
+                est_duration = 0.0
+            else:
+                est_duration = data_dict['onset_'] / float(frame_num)
+            data_dict['duration_'] = min(est_duration,
+                                         result.stim.duration - data_dict['onset_'])
+            data_dict['order_'] = frame_num
+            rows.append(data_dict)
+        return pd.DataFrame(rows)
