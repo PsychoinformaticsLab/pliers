21866e5cbb117cbd57f21db241f942c9960960d0
qmac <quinten.mcnamara@gmail.com>
2018-05-06 19:12:34 -0500

    moved pysrt to optional dependency design

--- pliers/stimuli/text.py
+++ pliers/stimuli/text.py
@@ -5,7 +5,10 @@ import pandas as pd
 from six import string_types
 from six.moves.urllib.request import urlopen
 from pliers.support.decorators import requires_nltk_corpus
+from pliers.utils import attempt_to_import, verify_dependencies
 from .base import Stim
+
+pysrt = attempt_to_import('pysrt')
 
 
 class TextStim(Stim):
@@ -147,15 +150,15 @@ class ComplexTextStim(Stim):
 
     def save(self, path):
         if path.endswith('srt'):
-            from pysrt import SubRipFile, SubRipItem, SubRipTime
+            verify_dependencies(['pysrt'])
+            from pysrt import SubRipFile, SubRipItem
             from datetime import time
 
             out = SubRipFile()
             for elem in self._elements:
                 start = time(*self._to_tup(elem.onset))
                 end = time(*self._to_tup(elem.onset + elem.duration))
-                item = SubRipItem(0, start, end, elem.text)
-                out.append(item)
+                out.append(SubRipItem(0, start, end, elem.text))
             out.save(path)
         else:
             with open(path, 'w') as f:
@@ -166,7 +169,7 @@ class ComplexTextStim(Stim):
                                                   elem.duration))
 
     def _from_srt(self, filename):
-        import pysrt
+        verify_dependencies(['pysrt'])
 
         data = pysrt.open(filename)
         list_ = [[] for _ in data]
