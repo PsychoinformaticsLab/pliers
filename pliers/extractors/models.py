dcbdaac435e15502afd6ad46fc3dd4e92f963eee
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-05-01 11:37:55 -0400

    mutiple minor enhancements - use attempt_to_import and verify_dependencies for tensorflow - remove unused imports - use bicubic resampling by default for image resizing - add note of resizing to object docstring

--- pliers/extractors/models.py
+++ pliers/extractors/models.py
@@ -1,26 +1,27 @@
 ''' Extractor classes based on pre-trained models. '''
 
-import os
-import tempfile
-import tarfile
-import subprocess
-import re
-import requests
+import numpy as np
+from PIL import Image
 from pliers.extractors.image import ImageExtractor
 from pliers.extractors.base import ExtractorResult
+from pliers.utils import attempt_to_import, verify_dependencies
+
+
+tf = attempt_to_import('tensorflow')
 
 
 def _resize_image(image, shape):
-    import numpy as np
-    from PIL import Image
     return np.array(
-        Image.fromarray(image).resize(shape, resample=Image.BILINEAR))
+        Image.fromarray(image).resize(shape, resample=Image.BICUBIC))
 
 
 class TensorFlowKerasInceptionV3Extractor(ImageExtractor):
 
     ''' Labels objects in images using a pretrained Inception V3 architecture
-     implemented in TensorFlow / Keras.
+    implemented in TensorFlow / Keras.
+
+    Images must be RGB and have shape (299, 299). Images will be resized (with
+    some distortion) if the shape is different.
 
     Args:
         weights (str): URL to download pre-trained weights. If None (default),
@@ -34,28 +35,27 @@ class TensorFlowKerasInceptionV3Extractor(ImageExtractor):
     VERSION = '1.0'
 
     def __init__(self, weights=None, num_predictions=5):
+        verify_dependencies(['tensorflow'])
         super(TensorFlowKerasInceptionV3Extractor, self).__init__()
         if weights is None:
             weights = 'imagenet'
         self.weights = weights
         self.num_predictions = num_predictions
-
-        import tensorflow as tf
         # Instantiating the model also downloads the weights to a cache.
         self.model = tf.keras.applications.inception_v3.InceptionV3(
             weights=self.weights)
 
     def _extract(self, stim):
-        import tensorflow as tf
-
         required_shape = (299, 299, 3)
         x = stim.data
         if x.ndim != 3:
-            raise ValueError("Stim must have rank 3.")
+            raise ValueError("Stim data must have rank 3 but got rank {}".format(x.ndim))
         if x.shape != required_shape:
             x = _resize_image(x, required_shape[:-1])
+        # Add batch dimension.
+        x = x[None]
+        # Normalize the features.
         x = tf.keras.applications.inception_v3.preprocess_input(x)
-        x = x[None]  # Add batch dimension.
         preds = self.model.predict(x, batch_size=1)
 
         # This produces a nested list. There is one sub-list per sample in the
@@ -64,7 +64,7 @@ class TensorFlowKerasInceptionV3Extractor(ImageExtractor):
         decoded = tf.keras.applications.inception_v3.decode_predictions(
             preds, top=self.num_predictions)
 
-        # We assume there is only one sample.
+        # We assume there is only one sample in the batch.
         decoded = decoded[0]
         values = [t[2] for t in decoded]
         features = [t[1] for t in decoded]
