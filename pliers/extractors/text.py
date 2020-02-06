05ca06d46200e550fe0d9e080d1d92c58ecde7c2
Roberta Rocca <32483140+rbroc@users.noreply.github.com>
2020-02-05 21:08:21 -0600

    Update pliers/extractors/text.py
    Co-Authored-By: Alejandro de la Vega <aleph4@gmail.com>

--- pliers/extractors/text.py
+++ pliers/extractors/text.py
@@ -14,7 +14,6 @@ from pliers.utils import (attempt_to_import, verify_dependencies, flatten,
 import numpy as np
 import pandas as pd
 import nltk
-import itertools
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import re
 import logging
