3daff0debaacd973a5a54881def62f2370c720f7
rbroc <rbrrcc@gmail.com>
2020-02-13 21:43:24 -0600

    variable name

--- pliers/tests/test_stims.py
+++ pliers/tests/test_stims.py
@@ -261,7 +261,7 @@ def test_transcribed_audio_stim():
 
 def test_remote_stims():
     video_url = 'https://archive.org/download/DisneyCastletest/Disney_Castle_512kb.mp4'
-    video = VideoStim(url=url)
+    video = VideoStim(url=video_url)
     assert video.fps == 30.0
 
     url = 'http://www.bobainsworth.com/wav/simpsons/themodyn.wav'
