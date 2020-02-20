1067380b910cfc9013e4b0b2888fea8e406cb120
rbroc <rbrrcc@gmail.com>
2020-02-19 22:11:48 -0600

    fix array flipping, check sampling rate and fix log_attributes

--- pliers/extractors/audio.py
+++ pliers/extractors/audio.py
@@ -513,7 +513,7 @@ class AudiosetLabelExtractor(AudioExtractor):
         parameters for the model (see params.py file in yamnet repository)
     '''
 
-    _log_attributes = ()
+    _log_attributes = ('hop_size', 'top_n', 'yamnet_kwargs')
 
     def __init__(self, hop_size=0.1, top_n=None, 
                  spectrogram=False, **yamnet_kwargs):
@@ -524,10 +524,10 @@ class AudiosetLabelExtractor(AudioExtractor):
         WEIGHTS_PATH = path.join(MODULE_PATH, 'yamnet.h5')
         LABELS_PATH = path.join(MODULE_PATH, 'yamnet_class_map.csv')
 
+        self.hop_size = hop_size
         self.params = yamnet.params
         self.params.PATCH_HOP_SECONDS = hop_size
         self.yamnet_kwargs = yamnet_kwargs
-        print(yamnet_kwargs)
         for par, v in self.yamnet_kwargs.items():
             if par in self.params.__dict__:
                 setattr(self.params, par, v)
@@ -549,11 +549,9 @@ class AudiosetLabelExtractor(AudioExtractor):
                                                   steps=1)
         self.spectrogram = spectrogram
         
-        ########## CHECK HERE ########## 
         idx = np.mean(preds,axis=0).argsort()
-        preds = np.flip(preds[:,idx],axis=0)[:,:self.top_n]
+        preds = np.fliplr(preds[:,idx])[:,:self.top_n]
         labels = [self.labels[i] for i in idx][::-1][:self.top_n]
-        ################################
 
         durations = params.PATCH_HOP_SECONDS
         onsets = np.arange(start=0, stop=stim.duration, step=durations)
@@ -563,8 +561,6 @@ class AudiosetLabelExtractor(AudioExtractor):
                                onsets=onsets, durations=durations,
                                orders=list(range(len(onsets))))
 
-# Check model values
 # Add pointer to installation instructions (install models, run test install - link, add to pythonpath)
 # Add length warning / batching
 # Migrate to models
-# Look into kwargs log
