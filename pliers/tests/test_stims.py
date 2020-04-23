4464ebb4a7175968b03ac648fdfc8e08555823b5
rbroc <rbrrcc@gmail.com>
2020-04-23 11:49:30 +0200

    check test with new file

--- pliers/tests/test_stims.py
+++ pliers/tests/test_stims.py
@@ -281,7 +281,7 @@ def test_remote_stims():
     url = 'https://raw.githubusercontent.com/rbroc/pliers/prob_extractor/pliers/tests/data/vector/vector_df.txt'
     vec = VectorStim(url=url)
     assert vec.array.shape[0] == 20
-    assert len(vec.labels) == 5
+    assert len(vec.labels) == 20
 
 
 def test_get_filename():
