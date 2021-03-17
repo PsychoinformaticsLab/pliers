baf4adad8132e0def6b3d496d58a962eb8ccddff
Alejandro de la Vega <aleph4@gmail.com>
2021-03-17 17:37:33 -0500

    Try sys path insert once more

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,7 @@ import os
 import sys
 import glob
 
-sys.path.insert(0, os.path.abspath('.'))
-
-print(os.path.abspath('.'))
-print(glob.glob('../*'))
-
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
