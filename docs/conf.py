e05465657c6738f02eafdd851cfa74c6d6bbe439
Alejandro de la Vega <aleph4@gmail.com>
2021-03-17 17:28:39 -0500

    Print path

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 print(os.path.abspath('.'))
 print(glob('*'))
+print(os.path.dirname(os.path.realpath(__file__)))
 
 
 # -- General configuration ------------------------------------------------
