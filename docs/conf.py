7f0e0363faf7f5919dae3694867a35995165fc09
Alejandro de la Vega <aleph4@gmail.com>
2021-03-17 17:26:22 -0500

    DEBUG: Print glob

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 print(os.path.abspath('.'))
+print(glob('*'))
 
 
 # -- General configuration ------------------------------------------------
