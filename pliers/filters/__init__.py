4f3024b5c4ef2277974d0e0d8d9c52bd69d4cf52
rbroc <rbrrcc@gmail.com>
2021-10-29 15:25:48 -0500

    add image rescaling filter

--- pliers/filters/__init__.py
+++ pliers/filters/__init__.py
@@ -8,7 +8,8 @@ from .audio import (AudioTrimmingFilter,
 from .base import TemporalTrimmingFilter
 from .image import (ImageCroppingFilter,
                     ImageResizingFilter,
-                    PillowImageFilter)
+                    PillowImageFilter,
+                    ImageRescalingFilter)
 from .text import (WordStemmingFilter,
                    TokenizingFilter,
                    TokenRemovalFilter,
@@ -25,6 +26,7 @@ __all__ = [
     'ImageCroppingFilter',
     'ImageResizingFilter',
     'PillowImageFilter',
+    'ImageRescalingFilter',
     'WordStemmingFilter',
     'TokenizingFilter',
     'TokenRemovalFilter',
