18b753de1aee719c7c1d622235860272d066c7e6
Alejandro de la Vega <aleph4@gmail.com>
2022-11-14 16:59:36 -0600

    Remove all archive.org

--- pliers/tests/test_io.py
+++ pliers/tests/test_io.py
@@ -19,10 +19,11 @@ def test_magic_loader():
 
 def test_magic_loader2():
     text_file = join(get_test_data_path(), 'text', 'sample_text.txt')
-    video_url = 'https://archive.org/download/DisneyCastletest/Disney_Castle_512kb.mp4'
-    audio_url = 'https://archive.org/download/999WavFiles/TANKEN.WAV'
-    image_url = 'https://archive.org/download/NIX-C-1987-11903/1987_11903L.jpg'
+    video_url = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    audio_url = 'https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav'
+    image_url = 'https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg'
     text_url = 'https://github.com/psychoinformaticslab/pliers/blob/master/README.rst'
+
     stims = load_stims([text_file, video_url, audio_url, image_url, text_url])
     assert len(stims) == 5
     assert stims[1].fps == 30.0
